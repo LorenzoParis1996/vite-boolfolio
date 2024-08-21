929555c54a2a1fc79f5fcd109dc03ef583a9ecb0
LorezoParis1996 <lorenzoparis86@live.it>
2024-08-21 15:54:17 +0200

    testato chiamata api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,15 +1,45 @@
 <script>
-export default {
-    data() {
-        return {
+import axios from 'axios';
 
+export default{
+    data() {
+        return{
+            projects: [],
         }
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects/', {
+                params: {
+                    
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.results.data);
+                    this.projects = response.data.results.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+        }
+    },
+    created(){
+        this.getProjects();
     }
 }
 </script>
 
 <template>
   <h1>Content</h1>
+
+  <ul>
+    <li v-for="project in projects">
+      {{ project.title }}
+    </li>
+  </ul>
 </template>
 
 <style lang="scss" scoped>
