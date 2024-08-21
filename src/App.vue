e15eb982d7f9f1befe4c73874e63144a941d3e73
LorezoParis1996 <lorenzoparis86@live.it>
2024-08-21 15:04:24 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import MainHeader from './components/MainHeader.vue';
+import AppMain from './components/AppMain.vue';
+import MainFooter from './components/MainFooter.vue';
+
+
+export default {
+    components: {
+      MainHeader,
+      AppMain,
+      MainFooter
+    }
+}
+
+</script>
+
+<template>
+ <MainHeader/>
+ <AppMain/>
+ <MainFooter/>
+</template>
+
+<style lang="scss">
+ @use "./styles/general.scss";
+ 
+</style>
